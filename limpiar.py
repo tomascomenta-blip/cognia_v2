3372d64e293e4f27a811662552938deed45314a1
tomascomenta-blip <tomascomenta@gmail.com>
2026-04-21 21:26:28 -0500

    chore: limpieza estructural - eliminar parches, backups y duplicados

--- limpiar.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('cognia_memory.db')
-c = conn.execute("SELECT id, label, observation FROM episodic_memory WHERE label LIKE '%volleyball%' OR label LIKE '%realsports%'")
-for row in c.fetchall():
-    print(row[0], row[1], row[2][:50])
-conn.execute("DELETE FROM episodic_memory WHERE label LIKE '%volleyball%' OR label LIKE '%realsports%'")
-conn.commit()
-print('Listo')
-conn.close()
